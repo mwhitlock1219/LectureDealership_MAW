4796bfeb2abe42084696aeccbafbd6c2332d9838
Melina Alejos <malejos1219@gmail.com>
2020-05-20 10:45:15 -0400

    Fix add Department and ServiceDepartment files

---
+++ ServiceDepartment.java
@@ -0,0 +1,20 @@
+package LectureDealership;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class ServiceDepartment extends Department {
+
+    public ServiceDepartment() {
+        super("Service", 20);
+    }
+
+    @Override
+    public boolean isOpenToday(LocalDateTime day) {
+        if (day.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
