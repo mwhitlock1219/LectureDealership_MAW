d43f82a80d222a17a3db869fb3a2ef737169f27a
Melina Alejos <malejos1219@gmail.com>
2020-05-21 08:12:30 -0400

    Add Leaseable.java and Promotable.java interfaces

---
+++ Leaseable.java
@@ -0,0 +1,10 @@
+//Interfaces normally follow a naming convention similar to classes but the end in 'ible' or 'able'
+// Usually adjective b/c we usually use interfaces to provide additional capabilities
+
+interface Leaseable {
+    boolean isLeaseable();
+
+    int getLeaseTerm();
+
+    int getMaxMilesPerYear();
+}
