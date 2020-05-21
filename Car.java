2597990c11404ee5f3a357dbe42b1555fb8dc09d
Melina Alejos <malejos1219@gmail.com>
2020-05-21 08:16:27 -0400

    Implement Leaseable interface in Car class

--- Car.java
+++ Car.java
@@ -1,6 +1,7 @@
 package LectureDealership;
 
-public class Car extends Vehicle {
+// Convention dictates that "imlements" come after "extends" if present
+public class Car extends Vehicle implements Leaseable {
 
     // Simple contructor inheriting all the methods an properties of Vehicle
     public Car(String VIN, double wholesaleCost, double retailPrice, int modelYear, String make, String model,
@@ -13,5 +14,18 @@ public class Car extends Vehicle {
         return super.getTargetMargin() - 1000;
         // use the getTargetMargin method as its defined in our super class (Vehicle)
         // same as == .getRetailPrice() - this.getWholesaleCost() - 1000;
+    }
+
+    @Override
+    public boolean isLeaseable() {
+        return true;
+    }
+
+    public in getLeaseTerm() {
+        return 24;
+    }
+
+    public int getMaxMilesPerYear() {
+        return 15_000;
     }
 }
